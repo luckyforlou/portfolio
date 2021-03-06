b2bfccd9288ab60bc50fec6e0edb6958ec6e89fa
Sabrina Ales <sabrina-ales@hotmail.com>
2021-03-06 18:28:21 -0500

    lien accueil

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,14 @@
 import './Header.scss';
 import NavBar from './Nav';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+
 
 export default function Header() {
 return (
+  <Router>
 <section id="header">
 
   
@@ -10,7 +16,7 @@ return (
   <div className=" navBar container mx-auto p-5">
     <nav uk-sticky="show-on-up: true; animation: uk-animation-slide-top;" className="flex justify-between">
       <div>
-        <a href="#">Logo</a>
+      <Link to="/">Logo</Link>
       </div>
       <NavBar></NavBar>
       
@@ -20,6 +26,6 @@ return (
 
   <div uk-parallax="bgy: -200" className="image"></div>
 
-</section>
+</section></Router>
 );
 }
