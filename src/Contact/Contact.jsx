7b681d955413db28bad3ea2db4799764e4e8f0cd
Sabrina Ales <sabrina-ales@hotmail.com>
2021-03-06 19:09:17 -0500

    ajout pages

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import './Contact.scss';
 export default function Contact(){
     return(
         <section id="contact">
+          <h1>CONTAAAAAAAACT</h1>
       </section>
     );
 }
---
+++ src/Portfolio/Portfolio.jsx
@@ -0,0 +1,5 @@
+export default function Portfolio() {
+    return (
+      <h1>POOOOOORTFOLIOOOOOOOOOO</h1>
+    );
+    }
