959e9a04c7d0985ad907af9d71051ffc0ab3c545
Sabrina Ales <sabrina-ales@hotmail.com>
2021-03-06 19:51:13 -0500

    fix lien description

--- src/Commun/Nav.jsx
+++ src/Commun/Nav.jsx
@@ -3,13 +3,14 @@ import {
   Link
 } from "react-router-dom";
 import NavBar from '../Commun/NavBurger';
+import './Nav.scss';
 
 export default function HeaderIndex() {
 return (
 
 
 
-<div className=" navBar container mx-auto p-5">
+<div className=" navBar container">
     <nav uk-sticky="show-on-up: true; animation: uk-animation-slide-top;" className="flex justify-between">
       <div>
       <Link to="/">Logo</Link>
--- src/Index/AboutIndexText.jsx
+++ src/Index/AboutIndexText.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import './AboutIndexText.scss';
+
 
 export default function AboutIndexText(){
     return(
@@ -7,7 +9,8 @@ export default function AboutIndexText(){
             <p>I am an illustrator and concept artist based in Montréal, Canada. </p>
            <p> I've worked as a freelance artist for over 5 years.
             Although my favourite subjects are environments and backgrounds, I also enjoy creating characters and props.</p>
-            <a href="#" className="aboutPlus"><p>Learn more</p></a>
+            <Link to="/about">Learn more</Link>
+            
         </div>
     );
 }
