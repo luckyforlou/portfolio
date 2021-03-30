a040f58c33c297c136e92e576119a899f0aef9bf
Sabrina Ales <sabrina-ales@hotmail.com>
2021-03-30 10:41:13 -0400

    finalisations

--- src/Commun/Nav.jsx
+++ src/Commun/Nav.jsx
@@ -1,26 +1,21 @@
-
-import {
-  Link
-} from "react-router-dom";
-import NavBar from '../Commun/NavBurger';
-import './Nav.scss';
-import Logo from '../media/logo.gif'
+import { Link } from "react-router-dom";
+import NavBar from "../Commun/NavBurger";
+import "./Nav.scss";
+import Logo from "../media/logo.gif";
 export default function HeaderIndex() {
-
-return (
-
-
-
-<div className="w-full navBar container">
-  
-    <nav uk-sticky="show-on-up: true; animation: uk-animation-slide-top;" className="flex justify-between">
-      <div>
-      <Link to="/"><img className="w-20" src={Logo} alt="Ales"/></Link>
-      </div>
-      <NavBar></NavBar>
-      
-     
-    </nav> 
-  </div>
-);
+  return (
+    <div className="w-full navBar container">
+      <nav
+        uk-sticky="show-on-up: true; animation: uk-animation-slide-top;"
+        className=" w-full flex justify-between"
+      >
+        <div>
+          <Link to="/">
+            <img className="w-20" src={Logo} alt="Ales" />
+          </Link>
+        </div>
+        <NavBar></NavBar>
+      </nav>
+    </div>
+  );
 }
--- src/Commun/NavBurger.jsx
+++ src/Commun/NavBurger.jsx
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
- 
-import ResponsiveMenu from 'react-responsive-navbar';
-import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
-import './NavBurger.scss';
+import React, { Component } from "react";
 
-import {
-  Link
-} from "react-router-dom";
-const Mailto = ({ email, subject = '', body = '', children }) => {
-  let params = subject || body ? '?' : '';
+import ResponsiveMenu from "react-responsive-navbar";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import "./NavBurger.scss";
+
+import { Link } from "react-router-dom";
+const Mailto = ({ email, subject = "", body = "", children }) => {
+  let params = subject || body ? "?" : "";
   if (subject) params += `subject=${encodeURIComponent(subject)}`;
-  if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+  if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
 
   return <a href={`mailto:${email}${params}`}>{children}</a>;
 };
 export default class Nav extends Component {
   render() {
     return (
-      
       <ResponsiveMenu
-        menuOpenButton={<MenuIcon/>}
-        menuCloseButton={<CloseIcon/>}
-        changeMenuOn="500px"
+        menuOpenButton={<MenuIcon />}
+        menuCloseButton={<CloseIcon />}
+        changeMenuOn="600px"
         largeMenuClassName="flex largeMenu"
         smallMenuClassName="flex smallMenu"
         menu={
-            <ul>
-              <li><Link to="/about">À Propos</Link></li>
-              <li><Link to="/portfolio">Portfolio</Link></li>
-              <li id="contactLink"><Mailto email="sabrina-ales@hotmail.com">Contact< span uk-icon="mail"></span></Mailto></li>
-            </ul>
+          <ul>
+            <li>
+              <Link to="/about">À Propos</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li id="contactLink">
+              <Mailto email="sabrina-ales@hotmail.com">
+                Contact<span uk-icon="mail"></span>
+              </Mailto>
+            </li>
+          </ul>
         }
       />
-    
-  );
+    );
+  }
 }
-}
-
-
